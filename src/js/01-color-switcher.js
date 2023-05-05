866cba7a456d4349736f7c2ef7557b7055a7984b
LuisETeguaS <tegualuis2@gmail.com>
2023-05-05 08:42:45 -0500

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const stop = document.querySelector('[data-stop]');
+const start = document.querySelector('[data-start]');
+const body = document.querySelector('body');
+let timerColor;
 
+const asignarColor = () => body.style.background = getRandomHexColor();
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startEvent = () => {
+  timerColor = setInterval(asignarColor, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+}
+
+const stopEvent = () => {
+  clearInterval(timerColor);
+  start.disabled = false;
+  stop.disabled = true;
+}
+
+stop.disabled = true;
+start.addEventListener('click', startEvent);
+stop.addEventListener('click', stopEvent);
