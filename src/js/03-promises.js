867885877209581e14f2d482379f75c3c4006fc8
LuisETeguaS <tegualuis2@gmail.com>
2023-05-09 10:30:31 -0500

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+let info = {};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  if (shouldResolve)
+  return Promise.resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  return Promise.reject(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function addPromise() {
+  let time = info.delay;
+  for (let i = 1; i <= info.amount; i++) {
+    setTimeout(() => {
+      createPromise(i, info.delay)
+        .then(greeting => console.log(greeting))
+        .catch(error => console.log(error));
+        info.delay += info.step;
+    }, time);
+    time += info.step;
   }
 }
+
+function submit(e) {
+  const inputs = form.querySelectorAll('input');
+  inputs.forEach(input => {
+    info[input.name] = parseFloat(input.value);
+  });
+  e.preventDefault();
+  addPromise();
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', submit);
+
